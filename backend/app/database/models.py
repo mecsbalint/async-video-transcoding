f7771f1603eea8dfe6f8c6c3069f13b9abd52bd3
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2026-02-11 14:08:31 +0100

    Implement database model structure

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,11 +1,24 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.database.base import Base
 
 
-class TestTable(Base):
-    __tablename__ = "testtabletwo"
+class Job(Base):
+    __tablename__ = "job"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    state = Column(String)
+    original_url = Column(String)
+    preview_url = Column(String)
+    thumbnail_url = Column(String)
+    original_video_metadata = relationship("MetaData", uselist=False, back_populates="job")
+
+
+class MetaData(Base):
+    __tablename__ = "metadata"
+
+    id = Column(Integer, primary_key=True)
+    duration = Column(Integer)
+    fps = Column(Integer)
+    codec = Column(String)
+    job = relationship("Job", uselist=False, back_populates="original_video_metadata")
