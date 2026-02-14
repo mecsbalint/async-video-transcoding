c58eadc3f90fcbd82f8197afadc92c13eeb37ab4
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2026-02-14 09:55:19 +0100

    Bugfix endpoints function names

--- backend/app/api/endpoints.py
+++ backend/app/api/endpoints.py
@@ -2,7 +2,7 @@ from flask import Flask, Response, request, jsonify
 from app.api.services import upload_video as upload_video_service, get_job as get_job_service
 
 
-def is_allowed_file(filename: str | None) -> bool:
+def __is_allowed_file(filename: str | None) -> bool:
     from app.api import ALLOWED_EXTENSIONS
     if filename is None:
         return False
@@ -26,7 +26,7 @@ def init_endpoints(app: Flask):
             # TODO: implement error handling
             return Response(status=400)
 
-        if not video or not is_allowed_file(video.filename):
+        if not video or not __is_allowed_file(video.filename):
             # TODO: implement error handling
             return Response(status=400)
 
