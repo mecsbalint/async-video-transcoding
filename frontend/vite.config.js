5d07bab130c5da8964e330f9f5bdc40905fb2566
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2026-02-17 11:41:02 +0100

    Remove env var using in Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const env = loadEnv(process.cwd(), "");
-const BACKEND_URI = env.VITE_BACKEND_URI;
-
 // https://vite.dev/config/
 export default defineConfig({
     server: {
         proxy: {
-            "/api": BACKEND_URI
+            "/api": "http://localhost:5000/"
         },
     },
     plugins: [
