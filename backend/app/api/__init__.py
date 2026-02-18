80775b7028fe948b796e795af1c19a2f1b4de1a9
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2026-02-18 22:50:55 +0100

    Implement Flask error handling

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.api.endpoints import init_endpoints
 from app.folders import UPLOAD_FOLDER_PATH, STATIC_FOLDER_URL
+from app.api.flask_error_handlers import init_error_handlers
 
 
 def init_api():
@@ -8,5 +9,7 @@ def init_api():
     app = Flask(__name__, static_folder=UPLOAD_FOLDER_PATH, static_url_path=STATIC_FOLDER_URL)
 
     init_endpoints(app)
+
+    init_error_handlers(app)
 
     return app
