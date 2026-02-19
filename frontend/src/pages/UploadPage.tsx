2ed0a3a134a424605e07406e8492b66c609aa4e3
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2026-02-19 11:06:54 +0100

    Implement error displaying on frontend UploadPage

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 function UploadPage() {
     const [file, setFile] = useState<File | null>(null);
     const [priorityHigh, setPriorityHigh] = useState<boolean>(false);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
     const navigate = useNavigate();
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -13,6 +14,10 @@ function UploadPage() {
         uploadVideo(file as File, priorityHigh).then(response => {
             if (response.status === 201) {
                 navigate("/joblist");
+            } else if (response.status === 400) {
+                setErrorMsg(`Error: ${response.body.error}`);
+            } else if (response.status >= 400 && response.status <= 599) {
+                setErrorMsg(`Error: Something went wrong during the process`);
             }
             setFile(null);
         });
@@ -24,6 +29,7 @@ function UploadPage() {
             <input type="file" className="file-input" onChange={event => {
                 if (event.target.files !== null) {
                     setFile(event.target.files[0]);
+                    setErrorMsg(null);
                 }
                 }} />
             <div>
@@ -31,9 +37,12 @@ function UploadPage() {
                 <input type="checkbox" onChange={event => setPriorityHigh(event.target.checked)} />
             </div>
             <div>
-                <button type="submit" className="btn btn-primary mt-2" disabled={file === null}>
+                <button type="submit" className="btn btn-primary mt-2" disabled={file === null || errorMsg !== null}>
                     Upload video
                 </button>
+            </div>
+            <div className={`${errorMsg === null ? "hidden" : ""}`}>
+                <p className="text-error">{errorMsg}</p>
             </div>
         </form>
     )
